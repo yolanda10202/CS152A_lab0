154f07401fde12cba3b8c81bb56bd5b44819e7a0
yolanda10202 <66993349+yolanda10202@users.noreply.github.com>
2022-04-06 12:36:50 -0700

    Update counter_TB.v

--- counter_TB.v
+++ counter_TB.v
@@ -1,24 +1,44 @@
-// Code your testbench here
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:08:43 04/06/2022 
+// Design Name: 
+// Module Name:    counter_TB 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
 // or browse Examples
 module counter_TB();
   reg clk, rst;
-  reg [3:0] result;
+  wire [3:0] result;
   
   counter m_counter(.clk(clk), .rst(rst), .a(result));
   
   initial begin
     clk = 1'b0;
     rst = 1'b0;
-    // result = 4'b0000;
+	 //$display(result);
     #5 rst = 1;
+	 //$display(result);
     #5 rst = 0;
-    
+	 //$display(result);
+    #1000
+	 $finish;
   end
   
   always begin
-    repeat(10) begin
-      #100;
-      clk <= ~clk;
-    end
+      #1 clk <= ~clk;
   end
 endmodule
+
