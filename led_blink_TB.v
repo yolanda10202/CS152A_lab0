c97970d88f4b34cdef3da7beb69f137531d46352
yolanda10202 <66993349+yolanda10202@users.noreply.github.com>
2022-04-06 13:09:55 -0700

    Update led_blink_TB.v

--- led_blink_TB.v
+++ led_blink_TB.v
@@ -1,12 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:05:17 04/06/2022 
+// Design Name: 
+// Module Name:    led_blink_TB 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
 module led_blink_TB();
   reg clk;
-  wire a;
+  wire [7:0] a;
   
-  led_blink m_led(.clk(clk), .a(a));
+  led_blink m_led(.clk(clk), .Led(a));
   
   initial begin
     clk = 1'b0;
-	  a = 1'b0;
   end
   
   always begin
